4b5f2b49ae2e6bf0da93397ac7ec361e7a83ca46
Belf <sleven182@mail.ru>
2021-02-22 00:16:38 +0300

    Add Lesson2

--- src/main/java/Basic.java
+++ src/main/java/Basic.java
@@ -1,8 +1,47 @@
 public class Basic {
     public static void main(String[] args) {
 
-
+        String[][] arr = {{"1","1","1","1"},{"1","1","1","1"},{"1","1","1","1"},{"1","1","1","1"}};
+        try {
+            System.out.println(myException(arr));
+        } catch (MyArraySizeException e) {
+            System.out.println(e.getMessage());
+        } catch (MyArrayDataException e) {
+            System.out.println(e.getMessage());
+        }
 
 
     }
+
+    static int myException(String[][] arr) throws MyArraySizeException, MyArrayDataException{
+        if(arr.length != 4 || arr[0].length != 4 || arr[1].length != 4 || arr[2].length != 4 || arr[3].length != 4) throw new MyArraySizeException();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                int num;
+                try {
+                    num = Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    String result = "arr" +"["+ i +"]"+"["+ j +"]" + " = " + arr[i][j] + " - is not a number";
+                    throw new MyArrayDataException (result);
+                }
+                sum += num;
+            }
+        }
+        return sum;
+
+    }
+
+    static class MyArraySizeException extends Exception{
+        MyArraySizeException(){
+            super("Wrong array size!");
+        }
+    }
+
+    static class MyArrayDataException extends Exception{
+        MyArrayDataException(String massage){
+            super(massage);
+        }
+    }
+
 }
